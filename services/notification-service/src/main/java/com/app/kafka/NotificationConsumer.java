dbd1409926f6733793058ecc37855ed9e81b9308
SohaibKhan007 <sohaibkhanatwork@gmail.com>
2025-08-24 20:15:09 +0530

    building API-GATEWAY service

--- services/notification-service/src/main/java/com/app/kafka/NotificationConsumer.java
+++ services/notification-service/src/main/java/com/app/kafka/NotificationConsumer.java
@@ -1,10 +1,10 @@
 package com.app.kafka;
 
+import com.app.email.EmailService;
 import com.app.kafka.order.OrderConfirmation;
-import com.app.kafka.payment.PaymentConfirmation;
 import com.app.notification.Notification;
 import com.app.notification.NotificationRepository;
-import com.app.notification.NotificationType;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-import static com.app.notification.NotificationType.*;
-import static java.lang.String.*;
+import static com.app.notification.NotificationType.ORDER_CONFIRMATION;
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -21,9 +21,10 @@ import static java.lang.String.*;
 public class NotificationConsumer {
 
     private final NotificationRepository notificationRepository;
+    private final EmailService emailService;
 
     @KafkaListener(topics = "order-topic")
-    public void consumeOrderConfirmationNotification(OrderConfirmation orderConfirmation) {
+    public void consumeOrderConfirmationNotification(OrderConfirmation orderConfirmation) throws MessagingException {
         log.info(format("consuming the message from order-topic Topic:: %s", orderConfirmation));
         notificationRepository.save(
                 Notification.builder()
@@ -33,7 +34,14 @@ public class NotificationConsumer {
                         .build()
         );
 
-        var customerName = pay
+        var customerName = orderConfirmation.customer().firstName() + " " + orderConfirmation.customer().lastName();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totoalAmount(),
+                orderConfirmation.orderReference(),
+                orderConfirmation.products()
+        );
     }
 
 }
